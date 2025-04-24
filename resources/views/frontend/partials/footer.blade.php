d31f62b596135e8d260f5d90815f245e76ac4fd8
shantahimu <shantaisalam@gmail.com>
2025-04-24 22:15:42 +0600

    master, header, footer

--- resources/views/frontend/partials/footer.blade.php
+++ resources/views/frontend/partials/footer.blade.php
@@ -1,114 +1,134 @@
-<footer id="footer" class="mt-5">
-    <div class="container">
-        <div class="row d-flex flex-wrap justify-content-between py-5">
-            <div class="col-md-3 col-sm-6">
-                <div class="footer-menu footer-menu-001">
-                    <div class="footer-intro mb-4">
-                        <a href="index.html">
-                            <img src="images/main-logo.png" alt="logo">
-                        </a>
-                    </div>
-                    <p>Gravida massa volutpat aenean odio. Amet, turpis erat nullam fringilla elementum diam in. Nisi, purus
-                        vitae, ultrices nunc. Sit ac sit suscipit hendrerit.</p>
-                    <div class="social-links">
-                        <ul class="list-unstyled d-flex flex-wrap gap-3">
-                            <li>
-                                <a href="#" class="text-secondary">
-                                    <svg width="24" height="24" viewBox="0 0 24 24">
-                                        <use xlink:href="#facebook"></use>
-                                    </svg>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" class="text-secondary">
-                                    <svg width="24" height="24" viewBox="0 0 24 24">
-                                        <use xlink:href="#twitter"></use>
-                                    </svg>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" class="text-secondary">
-                                    <svg width="24" height="24" viewBox="0 0 24 24">
-                                        <use xlink:href="#youtube"></use>
-                                    </svg>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" class="text-secondary">
-                                    <svg width="24" height="24" viewBox="0 0 24 24">
-                                        <use xlink:href="#pinterest"></use>
-                                    </svg>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" class="text-secondary">
-                                    <svg width="24" height="24" viewBox="0 0 24 24">
-                                        <use xlink:href="#instagram"></use>
-                                    </svg>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
+ 
+<div class="footer">
+
+<div class="container">
+
+    <div class="row">
+
+        <div class="col-md-3 col-sm-3">
+
+            <div class="single-widget">
+
+                <h2 class="widget-title">About Us</h2>
+
+                <div class="widget-inner">
+
+                    <p>Office: 868 Fake Street, New York</p>
+
+                    <p>Phone: (08) 8827 633354</p>
+
+                    <p>Fax: 08) 08 4752 1499</p>
+
+                    <p>Email: <a href="/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="96fbf7fffad6eff9e3e4f2f9fbf7fff8b8f5f9fb">[email&#160;protected]</a></p>
+
+                    <img src="images/payment.png" alt="we accept">
+
                 </div>
+
             </div>
-            <div class="col-md-3 col-sm-6">
-                <div class="footer-menu footer-menu-002">
-                    <h5 class="widget-title text-uppercase mb-4">Quick Links</h5>
-                    <ul class="menu-list list-unstyled text-uppercase border-animation-left fs-6">
-                        <li class="menu-item"><a href="index.html" class="item-anchor">Home</a></li>
-                        <li class="menu-item"><a href="index.html" class="item-anchor">About</a></li>
-                        <li class="menu-item"><a href="blog.html" class="item-anchor">Services</a></li>
-                        <li class="menu-item"><a href="styles.html" class="item-anchor">Single item</a></li>
-                        <li class="menu-item"><a href="#" class="item-anchor">Contact</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col-md-3 col-sm-6">
-                <div class="footer-menu footer-menu-003">
-                    <h5 class="widget-title text-uppercase mb-4">Help & Info</h5>
-                    <ul class="menu-list list-unstyled text-uppercase border-animation-left fs-6">
-                        <li class="menu-item"><a href="#" class="item-anchor">Track Your Order</a></li>
-                        <li class="menu-item"><a href="#" class="item-anchor">Returns + Exchanges</a></li>
-                        <li class="menu-item"><a href="#" class="item-anchor">Shipping + Delivery</a></li>
-                        <li class="menu-item"><a href="#" class="item-anchor">Contact Us</a></li>
-                        <li class="menu-item"><a href="#" class="item-anchor">Find us easy</a></li>
-                        <li class="menu-item"><a href="index.html" class="item-anchor">Faqs</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col-md-3 col-sm-6">
-                <div class="footer-menu footer-menu-004 border-animation-left">
-                    <h5 class="widget-title text-uppercase mb-4">Contact Us</h5>
-                    <p>Do you have any questions or suggestions? <a href="mailto:contact@yourcompany.com"
-                        class="item-anchor">contact@yourcompany.com</a></p>
-                    <p>Do you need support? Give us a call. <a href="tel:+43 720 11 52 78" class="item-anchor">+43 720 11 52
-                        78</a></p>
-                </div>
-            </div>
+
         </div>
-    </div>
-    <div class="border-top py-4">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-6 d-flex flex-wrap">
-                    <div class="shipping">
-                        <span>We ship with:</span>
-                        <img src="images/arct-icon.png" alt="icon">
-                        <img src="images/dhl-logo.png" alt="icon">
-                    </div>
-                    <div class="payment-option">
-                        <span>Payment Option:</span>
-                        <img src="images/visa-card.png" alt="card">
-                        <img src="images/paypal-card.png" alt="card">
-                        <img src="images/master-card.png" alt="card">
-                    </div>
+
+        <div class="col-md-3 col-sm-3">
+
+            <div class="single-widget">
+
+                <h2 class="widget-title">Information</h2>
+
+                <div class="widget-inner">
+
+                    <ul>
+
+                        <li><a href="">Frequently Asked Question</a></li>
+
+                        <li><a href="">Terms and Condition</a></li>
+
+                        <li><a href="">Privacy Policy</a></li>
+
+                        <li><a href="">Customer Service</a></li>
+
+                        <li><a href="">Delivery Information</a></li>
+
+                        <li><a href="">Manufacturers</a></li>
+
+                    </ul>
+
                 </div>
-                <div class="col-md-6 text-end">
-                    <p>© Copyright 2022 Kaira. All rights reserved. Design by <a href="https://templatesjungle.com"
-                        target="_blank">TemplatesJungle</a> Distribution By <a href="https://themewagon.com"
-                        target="blank">ThemeWagon</a></p>
-                </div>
+
             </div>
+
         </div>
+
+        <div class="col-md-3 col-sm-3">
+
+            <div class="single-widget">
+
+                <h2 class="widget-title">Customer Care</h2>
+
+                <div class="widget-inner">
+
+                    <ul>
+
+                        <li><a href="">Contact Us</a></li>
+
+                        <li><a href="">Sitemap</a></li>
+
+                        <li><a href="">Gift Vouchers</a></li>
+
+                        <li><a href="">Live Chat 24x7</a></li>
+
+                    </ul>
+
+                </div>
+
+            </div>
+
+        </div>
+
+        <div class="col-md-3 col-sm-3">
+
+            <div class="single-widget">
+
+                <h2 class="widget-title">Our Services</h2>
+
+                <div class="widget-inner">
+
+                    <ul>
+
+                        <li><a href="">Shipping & Returns</a></li>
+
+                        <li><a href="">Secure Shopping</a></li>
+
+                        <li><a href="">International Shipping</a></li>
+
+                        <li><a href="">Affiliates</a></li>
+
+                        <li><a href="">Contact</a></li>
+
+                    </ul>
+
+                </div>
+
+            </div>
+
+        </div>
+
     </div>
-</footer>
+
+</div>
+
+</div>
+<div class="copyright text-center">
+
+<div class="container">
+
+    <div class="row">
+
+        <p class="copyright"> Shared by <i class="fa fa-love"></i><a href="https://bootstrapthemes.co">BootstrapThemes</a>
+</p>
+
+    </div>
+
+</div>
+
+</div>
--- resources/views/frontend/partials/header.blade.php
+++ resources/views/frontend/partials/header.blade.php
@@ -1,48 +1,665 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
-    <div class="container">
-        <a class="navbar-brand fw-bold text-primary" href="/">Kaira</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNavbar">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="mainNavbar">
-            <ul class="navbar-nav ms-auto">
-                <li class="nav-item">
-                    <a class="nav-link btn btn-outline-primary" href="{{route('webpage')}}">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link btn btn-outline-primary" href="#">About Us</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link btn btn-outline-primary" href="#">Contact Us</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link btn btn-outline-primary" href="#">Shop</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link btn btn-outline-primary" href="#">Cart</a>
-                </li>
+<style>
+  .no-padding {
+    padding: 0px;
+  }
 
-                @guest('customerGuard') <!-- Check if the user is not logged in -->
-                    <li class="nav-item">
-                        <a class="nav-link btn btn-outline-primary" href="{{ route('customer.login') }}">Login</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link btn btn-outline-primary" href="{{ route('reg') }}">Registration</a>
-                    </li>
-                @else <!-- If the user is logged in -->
-                    <li class="nav-item">
-                        <span class="nav-link text-danger">{{ auth('customerGuard')->user()->name }}</span>
-                    </li>
-                    <li class="nav-item">
-                        <form action="{{ route('logout.success') }}" method="POST">
-                            @csrf
-                            <button type="submit" class="nav-link btn btn-danger" style="border: none; padding: 0;">
-                                Logout
-                            </button>
-                        </form>
-                    </li>
-                @endguest
-            </ul>
+  .glyphicon-icon-rpad .glyphicon,
+  .glyphicon-icon-rpad .glyphicon.m8,
+  .fa-icon-rpad .fa,
+  .fa-icon-rpad .fa.m8 {
+    padding-right: 8px;
+  }
+
+  .glyphicon-icon-lpad .glyphicon,
+  .glyphicon-icon-lpad .glyphicon.m8,
+  .fa-icon-lpad .fa,
+  .fa-icon-lpad .fa.m8 {
+    padding-left: 8px;
+  }
+
+  .glyphicon-icon-rpad .glyphicon.m5,
+  .fa-icon-rpad .fa.m5 {
+    padding-right: 5px;
+  }
+
+  .glyphicon-icon-lpad .glyphicon.m5,
+  .fa-icon-lpad .fa.m5 {
+    padding-left: 5px;
+  }
+
+  .glyphicon-icon-rpad .glyphicon.m12,
+  .fa-icon-rpad .fa.m12 {
+    padding-right: 12px;
+  }
+
+  .glyphicon-icon-lpad .glyphicon.m12,
+  .fa-icon-lpad .fa.m12 {
+    padding-left: 12px;
+  }
+
+  .glyphicon-icon-rpad .glyphicon.m15,
+  .fa-icon-rpad .fa.m15 {
+    padding-right: 15px;
+  }
+
+  .glyphicon-icon-lpad .glyphicon.m15,
+  .fa-icon-lpad .fa.m15 {
+    padding-left: 15px;
+  }
+
+
+
+  ul.nav-menu-list-style .nav-header .menu-collapsible-icon {
+    position: absolute;
+    right: 3px;
+    top: 16px;
+    font-size: 9px;
+  }
+
+
+
+  ul.nav-menu-list-style {
+    margin: 0;
+  }
+
+  ul.nav-menu-list-style .nav-header {
+    border-top: 1px solid #FFFFFF;
+    border-bottom: 1px solid #e8e8e8;
+    display: block;
+    margin: 0;
+    line-height: 42px;
+    padding: 0 8px;
+    font-weight: 600;
+  }
+
+  ul.nav-menu-list-style>li {
+    position: relative;
+  }
+
+  ul.nav-menu-list-style>li a {
+    border-top: 1px solid #FFFFFF;
+    border-bottom: 1px solid #e8e8e8;
+    padding: 0 10px;
+    line-height: 32px;
+  }
+
+  ul.nav-menu-list-style>li:first-child a {}
+
+
+  ul.nav-menu-list-style {
+    list-style: none;
+    padding: 0px;
+    margin: 0px;
+  }
+
+  ul.nav-menu-list-style li .badge,
+  ul.nav-menu-list-style li .pull-right,
+  ul.nav-menu-list-style li span.badge,
+  ul.nav-menu-list-style li label.badge {
+    float: right;
+    margin-top: 7px;
+  }
+
+  ul.bullets {
+    list-style: inside disc
+  }
+
+  ul.numerics {
+    list-style: inside decimal
+  }
+
+  .ul.kas-icon-aero {}
+
+  ul.kas-icon-aero li a:before {
+    font-family: 'Glyphicons Halflings';
+    font-size: 9px;
+    content: "\e258";
+    padding-right: 8px;
+  }
+</style>
+<script href="//cdnjs.cloudflare.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+<div class="top-bar">
+
+  <div class="container">
+
+    <div class="row">
+
+      <div class="col-md-6">
+
+        <div class="social pull-left">
+
+          <ul>
+
+            <li><a href=""><i class="fa fa-facebook"></i></a></li>
+
+            <li><a href=""><i class="fa fa-twitter"></i></a></li>
+
+            <li><a href=""><i class="fa fa-google-plus"></i></a></li>
+
+            <li><a href=""><i class="fa fa-linkedin"></i></a></li>
+
+          </ul>
+
         </div>
+
+      </div>
+
+      <div class="col-md-6">
+
+        <div class="action pull-right">
+
+          <ul>
+
+         
+              </a><style>
+  .no-padding {
+    padding: 0px;
+  }
+
+  .glyphicon-icon-rpad .glyphicon,
+  .glyphicon-icon-rpad .glyphicon.m8,
+  .fa-icon-rpad .fa,
+  .fa-icon-rpad .fa.m8 {
+    padding-right: 8px;
+  }
+
+  .glyphicon-icon-lpad .glyphicon,
+  .glyphicon-icon-lpad .glyphicon.m8,
+  .fa-icon-lpad .fa,
+  .fa-icon-lpad .fa.m8 {
+    padding-left: 8px;
+  }
+
+  .glyphicon-icon-rpad .glyphicon.m5,
+  .fa-icon-rpad .fa.m5 {
+    padding-right: 5px;
+  }
+
+  .glyphicon-icon-lpad .glyphicon.m5,
+  .fa-icon-lpad .fa.m5 {
+    padding-left: 5px;
+  }
+
+  .glyphicon-icon-rpad .glyphicon.m12,
+  .fa-icon-rpad .fa.m12 {
+    padding-right: 12px;
+  }
+
+  .glyphicon-icon-lpad .glyphicon.m12,
+  .fa-icon-lpad .fa.m12 {
+    padding-left: 12px;
+  }
+
+  .glyphicon-icon-rpad .glyphicon.m15,
+  .fa-icon-rpad .fa.m15 {
+    padding-right: 15px;
+  }
+
+  .glyphicon-icon-lpad .glyphicon.m15,
+  .fa-icon-lpad .fa.m15 {
+    padding-left: 15px;
+  }
+
+
+
+  ul.nav-menu-list-style .nav-header .menu-collapsible-icon {
+    position: absolute;
+    right: 3px;
+    top: 16px;
+    font-size: 9px;
+  }
+
+
+
+  ul.nav-menu-list-style {
+    margin: 0;
+  }
+
+  ul.nav-menu-list-style .nav-header {
+    border-top: 1px solid #FFFFFF;
+    border-bottom: 1px solid #e8e8e8;
+    display: block;
+    margin: 0;
+    line-height: 42px;
+    padding: 0 8px;
+    font-weight: 600;
+  }
+
+  ul.nav-menu-list-style>li {
+    position: relative;
+  }
+
+  ul.nav-menu-list-style>li a {
+    border-top: 1px solid #FFFFFF;
+    border-bottom: 1px solid #e8e8e8;
+    padding: 0 10px;
+    line-height: 32px;
+  }
+
+  ul.nav-menu-list-style>li:first-child a {}
+
+
+  ul.nav-menu-list-style {
+    list-style: none;
+    padding: 0px;
+    margin: 0px;
+  }
+
+  ul.nav-menu-list-style li .badge,
+  ul.nav-menu-list-style li .pull-right,
+  ul.nav-menu-list-style li span.badge,
+  ul.nav-menu-list-style li label.badge {
+    float: right;
+    margin-top: 7px;
+  }
+
+  ul.bullets {
+    list-style: inside disc
+  }
+
+  ul.numerics {
+    list-style: inside decimal
+  }
+
+  .ul.kas-icon-aero {}
+
+  ul.kas-icon-aero li a:before {
+    font-family: 'Glyphicons Halflings';
+    font-size: 9px;
+    content: "\e258";
+    padding-right: 8px;
+  }
+</style>
+<script href="//cdnjs.cloudflare.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+<div class="top-bar">
+
+  <div class="container">
+
+    <div class="row">
+
+      <div class="col-md-6">
+
+        <div class="social pull-left">
+
+          <ul>
+
+            <li><a href=""><i class="fa fa-facebook"></i></a></li>
+
+            <li><a href=""><i class="fa fa-twitter"></i></a></li>
+
+            <li><a href=""><i class="fa fa-google-plus"></i></a></li>
+
+            <li><a href=""><i class="fa fa-linkedin"></i></a></li>
+
+          </ul>
+
+        </div>
+
+      </div>
+
+     
+
     </div>
-</nav>
+
+  </div>
+
+</div>
+
+<div class="header">
+
+  <div class="container">
+
+    <div class="row">
+
+      <div class="col-md-3 col-sm-4">
+
+        <div class="logo">
+
+          <a href="index.html">
+
+            <img src="#" alt="Online Shopping">
+
+          </a>
+
+        </div>
+
+      </div>
+
+      <div class="col-md-7 col-sm-5">
+
+        <div class="search-form">
+
+          <form class="navbar-form" role="search" action="#">
+
+            <div class="form-group">
+
+              <input name="search_key" value="#" type="text" class="form-control" placeholder="#">
+
+            </div>
+
+            <button type="submit" class="btn"><i class="fa fa-search"></i></button>
+
+          </form>
+
+        </div>
+
+      </div>
+
+      <div class="col-md-2 col-sm-3">
+
+        <div class="cart">
+
+          <div class="cart-icon">
+
+            <a href="#">
+              <i class="fa fa-shopping-cart"></i>
+            </a>
+
+          </div>
+
+          
+
+
+        </div>
+
+      </div>
+
+
+    </div>
+
+
+  </div>
+
+</div>
+
+<div class="navigation">
+
+  <nav class="navbar navbar-theme">
+
+    <div class="container">
+
+      <!-- Brand and toggle get grouped for better mobile display -->
+
+      <div class="navbar-header">
+
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false">
+
+          <span class="sr-only">Menu</span>
+
+          <span class="icon-bar"></span>
+
+          <span class="icon-bar"></span>
+
+          <span class="icon-bar"></span>
+
+        </button>
+
+      </div>
+
+      <div class="shop-category nav navbar-nav navbar-left">
+
+        <!-- Single button -->
+
+        <div class="btn-group">
+
+          <button type="button" class="btn btn-shop-category dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+
+            <span class="caret"></span>
+
+          </button>
+
+         
+         
+
+          <script>
+            $('.tree-toggle').click(function() {
+              $(this).parent().children('ul.tree').toggle(200);
+            });
+            $(function() {
+              $('.tree-toggle').parent().children('ul.tree').toggle(200);
+            })
+          </script>
+
+        </div>
+
+      </div>
+
+      <!-- Collect the nav links, forms, and other content for toggling -->
+
+      <div class="collapse navbar-collapse" id="navbar">
+
+        <ul class="nav navbar-nav navbar-right">
+
+          <li><a href="#">Home</a></li>
+
+          <li><a href="#">Blog</a></li>
+
+          <li>
+            <a href="#">All Products</a>
+          </li>
+
+          <li><a href="#">Features</a></li>
+
+          <li><a href="#">Media</a></li>
+
+          <li><a href="#">About Us</a></li>
+
+          <li><a href="#">Contact Us</a></li>
+
+        </ul>
+
+      </div><!-- /.navbar-collapse -->
+
+    </div><!-- /.container-fluid -->
+
+  </nav>
+
+</div>
+
+
+
+
+
+
+<!-- login Modal -->
+<div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel"> Customer Login</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+
+      @guest('customerGuard') <!-- If the customer is not logged in -->
+    <li class="nav-item">
+        <a class="nav-link btn btn-outline-primary me-2" href="{{ route('customer.login') }}">
+            <i class="bi bi-box-arrow-in-right me-1"></i> Login
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link btn btn-outline-success" href="{{ route('reg') }}">
+            <i class="bi bi-person-plus me-1"></i> Register
+        </a>
+    </li>
+@else <!-- If the customer is logged in -->
+    <li class="nav-item d-flex align-items-center text-primary fw-bold me-3">
+        <i class="bi bi-person-circle me-1"></i> {{ auth('customerGuard')->user()->name }}
+    </li>
+    <li class="nav-item">
+        <form action="{{ route('logout.success') }}" method="POST" class="d-inline">
+            @csrf
+            <button type="submit" class="nav-link btn btn-danger" style="border: none;">
+                <i class="bi bi-box-arrow-right me-1"></i> Logout
+            </button>
+        </form>
+    </li>
+@endguest
+
+
+            <li>
+
+              
+
+
+              <div>
+                <a class="btn" href="">
+                  <svg fill="red" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                    <path d="M12 4.435c-1.989-5.399-12-4.597-12 3.568 0 4.068 3.06 9.481 12 14.997 8.94-5.516 12-10.929 12-14.997 0-8.118-10-8.999-12-3.568z" />
+                  </svg>
+                 
+
+                </a>
+              </div>
+
+            </li>
+
+           
+          </ul>
+
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+</div>
+
+<div class="header">
+
+  <div class="container">
+
+    <div class="row">
+
+      <div class="col-md-3 col-sm-4">
+
+
+      </div>
+
+      <div class="col-md-7 col-sm-5">
+
+        <div class="search-form">
+
+          <form class="navbar-form" role="search" action="#">
+
+            
+
+            <button type="submit" class="btn"><i class="fa fa-search"></i></button>
+
+          </form>
+
+        </div>
+
+      </div>
+
+      <div class="col-md-2 col-sm-3">
+
+        <div class="cart">
+
+          <div class="cart-icon">
+
+            <a href="#">
+              <i class="shopping-cart"></i>
+            </a>
+
+          </div>
+
+          <div class="cart-text">
+
+            SHOPPING CART
+
+            <br>
+
+           
+
+      <div class="navbar-header">
+
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false">
+
+          <span class="sr-only">Menu</span>
+
+          <span class="icon-bar"></span>
+
+          <span class="icon-bar"></span>
+
+          <span class="icon-bar"></span>
+
+        </button>
+
+      </div>
+
+      <div class="shop-category nav navbar-nav navbar-left">
+
+        <!-- Single button -->
+
+        <div class="btn-group">
+
+          <button type="button" class="btn btn-shop-category dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+
+            <span class="caret"></span>
+
+          </button>
+
+         
+         
+
+          <script>
+            $('.tree-toggle').click(function() {
+              $(this).parent().children('ul.tree').toggle(200);
+            });
+            $(function() {
+              $('.tree-toggle').parent().children('ul.tree').toggle(200);
+            })
+          </script>
+
+        </div>
+
+      </div>
+
+      <!-- Collect the nav links, forms, and other content for toggling -->
+
+      <div class="collapse navbar-collapse" id="navbar">
+
+        <ul class="nav navbar-nav navbar-right">
+
+          <li><a href="#">Home</a></li>
+
+          <li><a href="#">Blog</a></li>
+
+          <li>
+            <a href="#">All Products</a>
+          </li>
+
+          <li><a href="#">Features</a></li>
+
+          <li><a href="#">Media</a></li>
+
+          <li><a href="#">About Us</a></li>
+
+          <li><a href="#">Contact Us</a></li>
+
+        </ul>
+
+      </div><!-- /.navbar-collapse -->
+
+    </div><!-- /.container-fluid -->
+
+  </nav>
+
+</div>
+
+
+
+
+
+
+
+
+        
+
+    </div>
+  </div>
+</div>
