a00782a02f1e975938badef6c54224ea1a6e7681
Samiul Himel <alamhpl11@gmail.com>
2025-09-13 18:49:45 +0600

    update

--- resources/views/emails/order_placed.blade.php
+++ resources/views/emails/order_placed.blade.php
@@ -23,7 +23,7 @@
             <td style="padding: 35px; color: #333; font-size: 16px; line-height: 1.7;">
               <h2 style="margin-top: 0; font-weight: 600; color:rgb(70, 80, 129);">Hi {{ $order->name }},</h2>
 
-              <p>Thank you for shopping with <strong>Easy Shop</strong>! We're happy to let you know that your order has been successfully received and is now being processed.</p>
+              <p>Thank you for shopping with <strong>Elegance Bd</strong>! We're happy to let you know that your order has been successfully received and is now being processed.</p>
 
               <h3 style="margin-bottom: 12px; color:rgb(33, 80, 105);">🛍️ Order Details:</h3>
               <table style="width: 100%; font-size: 15px; margin-bottom: 20px;">
@@ -49,16 +49,16 @@
 
               <p>If you have any questions, just reply to this email or contact our support team — we're here to help!</p>
 
-              <p style="margin-top: 35px;">Warm regards,<br><strong>Easy Shop Team</strong></p>
+              <p style="margin-top: 35px;">Warm regards,<br><strong>Elegance Bd Team</strong></p>
             </td>
           </tr>
 
           <!-- Footer -->
           <tr style="background-color: #f9f9f9;">
             <td style="padding: 20px; text-align: center; font-size: 13px; color: #999999;">
-              <p>Easy Shop<br>Uttara, Dhaka, Bangladesh</p>
-              <p>Email: easyshop@gmail.com | Phone: +880 1234-567890</p>
-              <p style="margin: 0;">&copy; {{ date('Y') }} Easy Shop. All rights reserved.</p>
+              <p>Elegance Bd<br>Uttara, Dhaka, Bangladesh</p>
+              <p>Email: elegancebd@gmail.com | Phone: +880 01795-053841</p>
+              <p style="margin: 0;">&copy; {{ date('Y') }} Elegance Bd. All rights reserved.</p>
             </td>
           </tr>
 
